bbc3048f21e19ddcff7a704a577c14d8ee0876bb
Farhan Momin <farhan.m.nafis2412@gmail.com>
2024-02-08 01:44:36 +1100

    sample backend server using Flask

---
+++ backendServer.py
@@ -0,0 +1,27 @@
+from flask import Flask, make_response
+import random
+import string
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/generate_number", methods=["GET"])
+def generate_number():
+    length = random.randint(1, 12)
+    characters = string.ascii_letters + string.digits
+    result = "".join(random.choice(characters) for _ in range(length))
+
+    response = {"result": result}
+
+    res = make_response(jsonify(response), 200)
+    res.headers["Content-Type"] = "application/json"
+    res.headers["Access-Control-Allow-Origin"] = "*"
+    res.headers["Access-Control-Allow-Methods"] = "GET"
+    res.headers["Access-Control-Allow-Headers"] = "Content-Type"
+
+    return res
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8000)
