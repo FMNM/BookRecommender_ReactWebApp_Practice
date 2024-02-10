f14f594d8fe6f561bda23de499874c531b4ad4c4
Farhan Momin <farhan.m.nafis2412@gmail.com>
2024-02-10 14:34:36 +1100

    Added in a .NET Core backend version as well, for practice

---
+++ my-app_APIs/Controllers/PasswordGeneratorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_app_APIs.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class PasswordGeneratorController : ControllerBase
+    {
+        [HttpGet("generate_password")]
+        public IActionResult GeneratePassword()
+        {
+            int length = new Random().Next(8, 13);
+            string characters = string.Concat(Enumerable.Range(65, 26).Select(x => (char)x)) +
+                                string.Concat(Enumerable.Range(97, 26).Select(x => (char)x)) +
+                                string.Concat(Enumerable.Range(48, 10).Select(x => (char)x)) +
+                                string.Concat("!@#$%^&*()");
+
+            string result = new(Enumerable.Repeat(characters, length)
+                .Select(s => s[new Random().Next(s.Length)]).ToArray());
+
+            var response = new { result };
+
+            return Ok(response);
+        }
+    }
+}
